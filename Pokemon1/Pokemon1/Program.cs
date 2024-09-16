033e933a0ffaa7ced729fbbfa2099d943cc1a26c
Xd6774 <mifix2847@gmail.com>
2024-09-16 21:20:37 +0400

    r0 создание первого покемона

---
+++ Pokemon1/Pokemon1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon1
+{
+    class pokemon
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+        public int Speed { get; set; }
+    
+   
+
+        public pokemon(string name, int hp, int attack, int defense, int specialAttack, int specialDefense, int speed)
+        {
+            Name = name;
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+            SpecialAttack = specialAttack;
+            SpecialDefense = specialDefense;
+            Speed = speed;
+            
+          
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            
+            Console.WriteLine($"Очки здоровья: {HP}");
+            Console.WriteLine($"Атака: {Attack}");
+            Console.WriteLine($"Защита: {Defense}");
+            Console.WriteLine($"Специальная атака: {SpecialAttack}");
+            Console.WriteLine($"Специальная защита: {SpecialDefense}");
+            Console.WriteLine($"Скорость: {Speed}");
+            
+        }
+
+        public class Sewaddle : pokemon
+        {
+            public Sewaddle() : base("Sewaddle", 40, 53, 70, 40, 60, 42)
+            {
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Sewaddle sewaddle = new Sewaddle();
+                sewaddle.PrintInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+}
